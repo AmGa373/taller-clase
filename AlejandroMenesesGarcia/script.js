bebc16e173c31b50c3803c9bccc980c0da629c12
Alejandro Meneses <garciamenesesale@gmail.com>
2025-11-13 11:44:40 -0500

    feat se realizo el segundo ejercicio del taller en clase, para el calculo del area de la pared

--- AlejandroMenesesGarcia/script.js
+++ AlejandroMenesesGarcia/script.js
@@ -11,3 +11,16 @@ let total = productoUno + productoDos + productoTres;
 console.log ("El total a pagar es: $" + total);
 alert ("El total a pagar es: $" + total);
 //--------------------------------------------------------------------------------//
+
+//--------------------------------------------------------------------------------//
+/* 2. Para un evento, necesito un banner. 
+Pide el ancho y el alto de una pared (en metros) y calcula su área
+para saber cuánto material comprar.*/
+
+let ancho = parseFloat(prompt("Ingrese el ancho de la pared"));
+let alto = parseFloat(prompt("Ingrese el alto de la pared"));
+
+let area = ancho * alto;
+console.log ("El area es: " + area + " m2");
+alert ("El area es: " + area + " m2");
+//--------------------------------------------------------------------------------//
