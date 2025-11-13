670afd7d408605c1c730ebf35e73b91124f33a5a
Alejandro Meneses <garciamenesesale@gmail.com>
2025-11-13 12:17:32 -0500

    feat se realizo el quinto ejercicio del taller en clase, para saber cual es la edad del usuario

--- AlejandroMenesesGarcia/script.js
+++ AlejandroMenesesGarcia/script.js
@@ -17,10 +17,10 @@ alert ("El total a pagar es: $" + total);
 Pide el ancho y el alto de una pared (en metros) y calcula su área
 para saber cuánto material comprar.*/
 
-let ancho_pared = parseFloat(prompt("Ingrese el ancho de la pared"));
-let alto_pared = parseFloat(prompt("Ingrese el alto de la pared"));
+let anchoPared = parseFloat(prompt("Ingrese el ancho de la pared"));
+let altoPared = parseFloat(prompt("Ingrese el alto de la pared"));
 
-let area = ancho_area * alto_area;
+let area = anchoPared * altoPared;
 console.log ("El area es: " + area + " m2");
 alert ("El area es: " + area + " m2");
 //--------------------------------------------------------------------------------//
@@ -29,10 +29,10 @@ alert ("El area es: " + area + " m2");
 /* 3. Debo calcular cuánto perímetro tiene un potrero rectangular. 
 Pide el largo y el ancho del potrero y muestra el resultado.*/
 
-let largo_potrero = parseFloat(prompt("Ingrese el largo del potrero"));
-let ancho_potrero = parseFloat(prompt("Ingrese el ancho del potrero"));
+let largoPotrero = parseFloat(prompt("Ingrese el largo del potrero"));
+let anchoPotrero = parseFloat(prompt("Ingrese el ancho del potrero"));
 
-let perimetro = 2 * (ancho_potrero + largo_potrero);
+let perimetro = 2 * (anchoPotrero + largoPotrero);
 console.log ("El potrero tiene un perimetro de: " + perimetro + " metros");
 alert ("El potrero tiene un perimetro de: " + perimetro + " metros");
 //--------------------------------------------------------------------------------//
@@ -45,4 +45,17 @@ let fahrenheit = parseFloat(prompt("Ingrese la temperatura en °F"));
 let celsius = (fahrenheit - 32) * 5/9;
 console.log ("La temperatura en grados celsius es: " + celsius + " C°");
 alert ("La temperatura en grados celsius es: " + celsius + " C°");
+//--------------------------------------------------------------------------------//
+
+//--------------------------------------------------------------------------------//
+/* 5. Necesito saber la edad de un usuario. 
+Pide su año de nacimiento y, asumiendo que el año actual es 2024, 
+calcula cuántos años tiene.*/
+
+let annoNacimiento = parseFloat(prompt("Ingrese su fecha de Nacimiento"));
+let annoActual = 2024;
+
+let edad = annoActual - annoNacimiento;
+console.log ("La edad del usuario es: " + edad + " años");
+alert ("La edad del usuario es: " + edad + " años");
 //--------------------------------------------------------------------------------//
